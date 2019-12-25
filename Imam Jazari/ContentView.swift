8ac31233b5b8bbfa286a60350fc6d5594c86dbb0
Zain Mahmood <zainy_106@hotmail.co.uk>
2019-12-25 21:39:09 +0000

    Initial Commit

---
+++ Imam Jazari/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Imam Jazari
+//
+//  Created by Zain Mahmood on 25/12/2019.
+//  Copyright © 2019 Investasphere. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
